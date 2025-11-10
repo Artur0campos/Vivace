5df7cc5bb5a1edbfb60899a54c0a0487359f0752
Artur0campos <arturcamposcamara@gmail.com>
2025-11-10 16:59:21 -0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+let nameInput = document.getElementById('input-name')
+let emailInput = document.getElementById('input-email')
+let numInput = document.getElementById('input-numero')
+let btn = document.getElementById('inscricao-btn')
+const msg = document.getElementById('mensagem')
+
 function inscricao(){
     window.location.href = "login.html"
 }
@@ -13,4 +19,59 @@ function aulas(){
 function planos(){
     window.location.href = "index.html#planos"
 }
+
+function validateName(name){
+    if(name.trim() === ""){
+        window.alert("Digite um Nome")
+        return false
+    }
+    else{
+        return true
+    }
+}
+
+function validateEmail(email){
+    if(email.trim().length === 0){
+        window.alert("Email vazio")
+        return false
+    }
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if(regex.test(email.trim())){
+        return true
+    }
+    window.alert("Email inválido");
+    return false
+
+}
+
+function validateTel(telefone){
+    if(telefone.trim().length === 0){
+        window.alert("Número vazio")
+        return false
+    }
+    const digitos = telefone.replace(/\D/g, '')
+    const regex = /^\d{8,11}$/
+    if(regex.test(digitos)){
+        return true
+    }
+     window.alert("Número de telefone inválido (deve ter 8 a 11 dígitos)");
+    return false;
+}
+
+
+btn.addEventListener('click',function(event){
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const num = numInput.value;
+    if(validateName(name) && validateEmail(email) && validateTel(num)){
+        msg.innerText = "Inscrição feita com sucesso"
+        localStorage.setItem("Name", name) 
+        localStorage.setItem("emai", email) 
+        localStorage.setItem("Tel", num) 
+        event.preventDefault();
+        
+    }else {
+        msg.innerText = "";
+    }
+})
 
