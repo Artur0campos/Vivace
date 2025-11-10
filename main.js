6eb18825e9fde26aae4a31669f5afc92808fb94d
Artur0campos <arturcamposcamara@gmail.com>
2025-11-10 15:10:13 -0300

    responsividade + ligações entre paginas com JS

---
+++ main.js
@@ -0,0 +1,15 @@
+function inscricao(){
+    window.location.href = "login.html"
+}
+
+function quemSomos(){
+    window.location.href = "index.html#quem-somos"
+}
+
+function aulas(){
+    window.location.href = "index.html#aulas"
+}
+
+function planos(){
+    window.location.href = "index.html#planos"
+}
